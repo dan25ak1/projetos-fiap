36456fac953c1cc98c7a8428c9e37dcad859a8fc
dan2501 <dcs2501@gmail.com>
2024-05-11 21:34:08 -0300

    commit teste arquivo test.py

--- PYTHON/test.py
+++ PYTHON/test.py
@@ -1,5 +1,5 @@
 #3
-num1 = float(input("Valor da compra: "))
+num1 = float(input("Valor de compras: "))
 
 pix = ((num1 * 10) / 100)
 debito = ((num1 * 5) / 100)
@@ -9,3 +9,5 @@ vezes4 = ((num1 * 8) / 100)
 
 print("A vista em dinheiro ou pix: " , num1 - pix , "\nA vista no débito: " , num1 - debito
 , "\nEm duas vezes, 2 parcelas de: ", vezes2 , "\nEm três vezes, 3 parcelas de: ", (num1 + vezes3) / 3 , "\nEm quatro vezes, 4 parcelas de: ", (num1 + vezes4) / 4)
+
+#teste
